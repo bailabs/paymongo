e961eb5924455a7a814b75486405013e4b4e6190
Chris Ian Fiel <ccfiel@gmail.com>
2020-04-12 21:52:58 +0800

    update pattern with the javascript package

--- paymongo/paymongo.py
+++ paymongo/paymongo.py
@@ -2,7 +2,7 @@ from .payment_method import PaymentMethod
 from .payment_intent import PaymentIntent
 from .payment import Payment
 from .source import Source
-
+from .web_hook import WebHook
 
 class Paymongo(object):
 
@@ -11,4 +11,4 @@ class Paymongo(object):
         self.payment_intents = PaymentIntent(secret_key)
         self.payments = Payment(secret_key)
         self.sources = Source(secret_key)
-
+        self.webhooks = WebHook(secret_key)
