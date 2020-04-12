8c35d7f856460d3f748fbc6b20a2b9d9ecc3ec8e
Chris Ian Fiel <ccfiel@gmail.com>
2020-04-12 22:23:33 +0800

    update pattern with the javascript package

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-version = '0.1.1dev'
+version = '0.1.2dev'
 
 with open('requirements.txt') as requirements:
     install_requires = requirements.read().split()
 
 setup(
-    name='paymongo',
-    description="Paymongo Python Library",
+    name="example-pkg-ccfiel",  # Replace with your own username
     version=version,
-    author='Chris Ian Fiel',
-    author_email='ccfiel@bai.ph',
-    packages=[
-        'paymongo'
+    author="Chris Ian Fiel",
+    author_email="ccfiel@bai.ph",
+    description="Python package for Paymongo API",
+    long_description="A lightweight Python client for Paymongo API https://github.com/bailabs/paymongo",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bailabs/paymongo",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
     install_requires=install_requires
 )
+
